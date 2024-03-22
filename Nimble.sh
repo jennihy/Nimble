4ed863ba03b0879092d1cd1728608f81f9f25555
smeb y <48400087+a3165458@users.noreply.github.com>
2024-03-22 16:35:35 +0800

    Update Nimble.sh

--- Nimble.sh
+++ Nimble.sh
@@ -53,7 +53,7 @@ function main_menu() {
     clear
     echo "请选择要执行的操作:"
     echo "1. 安装常规节点"
-    read -p "请输入选项（1-3）: " OPTION
+    read -p "请输入选项（1）: " OPTION
 
     case $OPTION in
     1) install_node ;;
